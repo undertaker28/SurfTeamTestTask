caef0715a6c15a390b23feb3c74ab9532848b489
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-02-02 22:23:12 +0300

    Initial Commit

---
+++ SurfTeamTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfTeamTestTask
+//
+//  Created by Pavel on 2.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
