007a156c72df0503d18de723d602578cc7f85398
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-02-02 23:11:30 +0300

    Delete Storyboard and add project structure

---
+++ SurfTeamTestTask/Applications/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SurfTeamTestTask
+//
+//  Created by Pavel on 2.02.23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
